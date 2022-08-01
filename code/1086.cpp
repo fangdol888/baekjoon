6862c74113022c14b5cae2905b5c0fa4fe1342c3
fangdol888 <fangdol888@gmail.com>
2022-08-01 21:53:17 +0900

    solved 9446 term project

---
+++ code/1086.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int n,k;
+string s[15];
+
+int main(){
+    cin >> n;
+    for(int i=0;i<n;i++){
+        cin >> s[i];
+    }
+    cin >> k;
+
+
+    return 0;
+}
--- code/9446.cpp
+++ code/9446.cpp
@@ -3,43 +3,54 @@
 using namespace std;
 int test;
 int n;
-int ans = -1;
+int ans = 0;
 vector<int> stu;
-vector<int> visit;
+vector<bool> visit;
+vector<bool> done;
+
 int sum =0 ;
 
-bool dfs(int par, int cur){
-    if(par == cur){
-        return true;
-    }else{
-        if(!visit[stu[cur]]){
-            visit[stu[cur]] = true;
-            return dfs(par, stu[cur]);
+void hasCycle(int node){
+    visit[node] = true;
+    int next = stu[node];
+
+    if(!visit[next]){
+        hasCycle(next);
+    }else if(!done[next]){
+        for(int i=next; i!=node; i=stu[i]){
+            ans++;
         }
-        return false;
+        ans++;
     }
-    
+
+    done[node] = true;
 }
 
 int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
     cin >> test;
+
     for(int t=0;t<test;t++){
         cin >> n;
         stu.resize(0);visit.resize(0);
         stu.resize(n+1);visit.resize(n+1);
-        sum = 0;
+        done.resize(0);done.resize(n+1);
+
+        ans = 0;
 
         for(int i=1;i<=n;i++){
             cin >> stu[i];   
         }
+
         for(int i=1;i<=n;i++){
-            if(i == stu[i] || dfs(i, stu[i])){
-                continue;
+            if(!visit[i]){
+                hasCycle(i);
             }
-            else sum++;
         }
-        ans = sum;
-        cout << ans << '\n';
+        cout << n - ans << '\n';
     }
+
     return 0;
 }
