a1956a81b39fb25b76c209fcd3e2baa7f35ea54a
fangdol888 <fangdol888@gmail.com>
2022-08-02 14:45:50 +0900

    solved 10942 palindrome?

--- code/10942.cpp
+++ code/10942.cpp
@@ -4,19 +4,41 @@ using namespace std;
 
 int n,m;
 vector<int> su;
+bool dp[2001][2001];
+
+void make(){
+
+    for(int i=0;i<=n;i++){
+        for(int j=0;j <= n-i;j++){
+            int row = j;
+            int col = i + j;
+            if(row == col) dp[row][col] = true;
+            else if(row + 1 > col - 1){ 
+                if(su[row] == su[col]) dp[row][col] = true;
+            }else{
+                dp[row][col] = dp[row+1][col-1] && (su[row] == su[col]);
+            }
+        }
+    }
+}
 
 int main(){
-    cin >> n;su.resize(n);
-    for(int i=0;i<n;i++){  
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    cin >> n;su.resize(n+1);
+    for(int i=1;i<=n;i++){  
         cin >> su[i];
     }
-    
+
+    make();
+
     cin >> m;
+
     for(int i=0;i<m;i++){
         int start, end;
         cin >> start >> end;
-
-
+        cout << dp[start][end] << '\n';
     }
 
     return 0;
